d31807997f6294a9a11a186d33919914b5363d25
elangovana <aparnaelangovan@gmail.com>
2015-11-08 22:37:48 +1100

    first submission

--- aristo/core/run_test_data.py
+++ aristo/core/run_test_data.py
@@ -2,15 +2,17 @@ from aristo.core.aristo_data import AristoData
 from aristo.core.similarity_pipeline import SimilarityPipeline
 from aristo.core.text_analyser import TextAnalyser
 import os
+import time
 
-def run_train_data(train_data_csv, test_data_csv):
+def run_test_data(train_data_csv, test_data_csv):
     aristo_train_data = AristoData(train_data_csv)
-    aristo_test_data = AristoData(test_data_csv)
+    aristo_test_data = AristoData(test_data_csv, rows_to_include= range(1,10))
     aristo_test_data.print_summary()
     aristo_train_data.print_summary()
     pipeline = SimilarityPipeline(train_data=aristo_train_data, test_data=aristo_test_data)
     pipeline.run_pipeline()
-    out_dir=os.path.join(os.path.dirname(__file__),"../../../outputdata/train_{}".format(time.strftime('%Y%m%d_%H%M%S')))
+
+    out_dir=os.path.join(os.path.dirname(__file__),"../../../outputdata/{}_{}".format("run_test_data", time.strftime('%Y%m%d_%H%M%S')))
 
     os.makedirs(out_dir)
     pipeline.write_to_disk((out_dir))
@@ -20,4 +22,4 @@ def run_train_data(train_data_csv, test_data_csv):
 
 
 
-run_train_data(os.path.join(os.path.dirname(__file__),"../../../inputdata/training_set.tsv"), os.path.join(os.path.dirname(__file__),"../../../inputdata/validation_set.tsv"))
+run_test_data(os.path.join(os.path.dirname(__file__),"../../../inputdata/training_set.tsv"), os.path.join(os.path.dirname(__file__),"../../../inputdata/validation_set.tsv"))
